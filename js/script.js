f88d82d143c2102fa4c0d3de6c176764dd5ce2bb
Богдан Котов <bbrudastyy@gmail.com>
2020-06-14 16:39:30 +0300

    Добавил js и анимацию

--- js/script.js
+++ js/script.js
@@ -2,12 +2,60 @@ var interest_btn = document.querySelector(".interest-btn");
 var modal = document.querySelector(".interest-form");
 var date_start = modal.querySelector(".date-start");
 var date_end = modal.querySelector(".date-end");
+var adults = modal.querySelector(".amount-adults");
+var children = modal.querySelector(".amount-children");
+
+var am = modal.querySelector(".minus-adults");
+var ap = modal.querySelector(".plus-adults");
+var cm = modal.querySelector(".minus-children");
+var cp = modal.querySelector(".plus-children");
+var count_adults = 2;
+var count_children = 0;
+
+var isStorageSupport = true;
+var storage_adults = "";
+var storage_children = "";
+
+try {
+    storage_adults = localStorage.getItem("");
+    storage_children = localStorage.getItem("");
+} catch (err) {
+    isStorageSupport = false;
+}
+am.addEventListener("click", function () {
+    count_adults = count_adults - 1;
+    if (count_adults < 0) {
+        count_adults = 0;
+    }
+    adults.value = count_adults;
+})
+
+ap.addEventListener("click", function () {
+    count_adults = count_adults + 1;
+    adults.value = count_adults;
+
+})
+cm.addEventListener("click", function () {
+    count_children = count_children - 1;
+    if (count_children < 0) {
+        count_children = 0;
+    }
+    children.value = count_children;
+})
+cp.addEventListener("click", function () {
+    count_children = count_children + 1;
+    children.value = count_children;
+})
 
 
 interest_btn.addEventListener("click", function (evt) {
     if (modal.classList.contains("interest-form-hidden")) {
         evt.preventDefault();
         modal.classList.remove("interest-form-hidden");
+        if (storage_adults || storage_children) {
+            adults.value = storage_adults;
+            children.value = storage_children;
+        }
 
     }
     else {
@@ -26,14 +74,19 @@ window.addEventListener("keydown", function (evt) {
 });
 
 modal.addEventListener("submit", function (evt) {
-    if ((date_start.value === "") || (date_end.value === "")) {
-        alert("Введите дату");
+    if ((!date_start.value || !date_end.value) || (count_adults === 0 && count_children === 0)) {
+        alert("Проверьте правильность заполненных данных");
         evt.preventDefault();
     }
     else {
         alert("Отправлено");
+        if (isStorageSupport) {
+            localStorage.setItem("Amount-adults", adults.value);
+            localStorage.setItem("Amount-children", children.value);
+        }
         evt.preventDefault();
     }
 });
+
 
 
